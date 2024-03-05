fdb4db71790906da78ed7a49ba0e7be981b59d72
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-05 11:19:44 +0200

    Html optimisation

--- the_speach_therapy_center/the_speach_therapy_center/web/urls.py
+++ the_speach_therapy_center/the_speach_therapy_center/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from the_speach_therapy_center.web.views import index
+from the_speach_therapy_center.web.views import index, about, contact
 
 urlpatterns = (
     path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 )
--- the_speach_therapy_center/the_speach_therapy_center/web/views.py
+++ the_speach_therapy_center/the_speach_therapy_center/web/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+
 def index(request):
     context = {}
     return render(request, template_name='web/index.html', context=context)
+
+
+def about(request):
+    context = {}
+    return render(request, template_name='web/about.html', context=context)
+
+
+def contact(request):
+    context = {}
+    return render(request, template_name='web/contact.html', context=context)
