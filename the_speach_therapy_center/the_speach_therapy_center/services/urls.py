ff790fbc9f2dd8375b8d805c156cf06e2756d7a6
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-10 08:22:17 +0200

    Content update
    -services section

--- the_speach_therapy_center/the_speach_therapy_center/services/urls.py
+++ the_speach_therapy_center/the_speach_therapy_center/services/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = ()
+from django.urls import path
+
+from the_speach_therapy_center.services.views import pediatric_service, adult_service, occupational_service
+
+urlpatterns = (
+    path('pediatric/', pediatric_service, name='pediatric'),
+    path('adult/', adult_service, name='adult'),
+    path('occupational/', occupational_service, name='occupational'),
+
+)
--- the_speach_therapy_center/the_speach_therapy_center/services/views.py
+++ the_speach_therapy_center/the_speach_therapy_center/services/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def pediatric_service(request):
+    context = {}
+    return render(request, template_name='services/pediatric.html', context=context)
+
+
+def adult_service(request):
+    context = {}
+    return render(request, template_name='services/adult.html', context=context)
+
+
+def occupational_service(request):
+    context = {}
+    return render(request, template_name='services/occupational.html', context=context)
