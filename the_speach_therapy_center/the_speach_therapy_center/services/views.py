a84436afb795e08c6d94ee5c6150ca18227101e4
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-19 14:22:43 +0200

    Getting started page update

--- the_speach_therapy_center/the_speach_therapy_center/services/views.py
+++ the_speach_therapy_center/the_speach_therapy_center/services/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from the_speach_therapy_center.accounts.models import Profile
 
 
 def getting_started(request):
-    user = request.user  # Assuming user is authenticated
+    user = request.user
     user_email = user.email
+    profile = Profile.objects.get(user=request.user)
+    full_name = profile.full_name
     context = {
         'user_email': user_email,
+        'full_name': full_name,
     }
     return render(request, template_name='services/getting_started.html', context=context)
 
