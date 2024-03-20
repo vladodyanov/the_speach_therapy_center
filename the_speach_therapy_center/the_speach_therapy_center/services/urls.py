b8739103cf2deeb171a68a480d2fb2e468814a34
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-20 14:02:03 +0200

    screenshots for readme file

--- the_speach_therapy_center/the_speach_therapy_center/services/urls.py
+++ the_speach_therapy_center/the_speach_therapy_center/services/urls.py
@@ -4,5 +4,11 @@ from the_speach_therapy_center.services.views import getting_started
 
 urlpatterns = (
     path('getstarted/', getting_started, name='getting_started'),
+    # path("<str:username>/pet/<slug:pet_slug>/",
+    #      include([
+    #          path("", PetDetailView.as_view(), name='details pet'),
+    #          path("edit/", PetEditView.as_view(), name='edit pet'),
+    #          path("delete/", PetDeleteView.as_view(), name='delete pet'),
+    #      ])),
 
 )
