548d816a42a7a948a0cce6c3631285afdaa2bad6
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-26 13:22:02 +0200

    patient records view

--- the_speach_therapy_center/the_speach_therapy_center/staff_panel/urls.py
+++ the_speach_therapy_center/the_speach_therapy_center/staff_panel/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+
+from the_speach_therapy_center.staff_panel.views import patient_records
+
 urlpatterns = (
+    path('patient_records/', patient_records, name='patient_records'),
 
 )
--- the_speach_therapy_center/the_speach_therapy_center/staff_panel/views.py
+++ the_speach_therapy_center/the_speach_therapy_center/staff_panel/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from the_speach_therapy_center.accounts.models import Profile
+
+
+def patient_records(request):
+    patients = Profile.objects.all()
+    # patients = [user for user in registered_users if not user.is_staff and user.is_superuser]
+
+    context = {
+        'patients': patients,
+    }
+    return render(request, template_name='staff_panel/patients_records.html', context=context)
