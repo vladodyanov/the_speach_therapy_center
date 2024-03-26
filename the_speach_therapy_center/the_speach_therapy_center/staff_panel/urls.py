196163b7e365672d3aaae46a2d1ca0cc2b2550bf
Vladimir Dyanov <133800606+vladodyanov@users.noreply.github.com>
2024-03-26 16:04:01 +0200

    Patient records view update

--- the_speach_therapy_center/the_speach_therapy_center/staff_panel/urls.py
+++ the_speach_therapy_center/the_speach_therapy_center/staff_panel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from the_speach_therapy_center.staff_panel.views import patient_records
+from the_speach_therapy_center.staff_panel.views import patient_records, patient_details
 
 urlpatterns = (
     path('patient_records/', patient_records, name='patient_records'),
+    path('patient_records/<int:pk>/', patient_details, name='patient_details')
 
 )
--- the_speach_therapy_center/the_speach_therapy_center/staff_panel/views.py
+++ the_speach_therapy_center/the_speach_therapy_center/staff_panel/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from the_speach_therapy_center.accounts.models import Profile
 
 
 def patient_records(request):
     patients = Profile.objects.all()
-    # patients = [user for user in registered_users if not user.is_staff and user.is_superuser]
-
     context = {
         'patients': patients,
     }
     return render(request, template_name='staff_panel/patients_records.html', context=context)
+
+
+def patient_details(request, pk):
+    patient = get_object_or_404(Profile, pk=pk)
+    context = {
+        'patient': patient,
+    }
+    return render(request, 'staff_panel/patient_details.html', context=context)
+
